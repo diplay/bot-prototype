c1ee7c4f8741700e80ac7f300a7193957cd0303b
diplay <diplaySuper@gmail.com>
2016-12-15 02:47:19 +0300

    some shit works

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 var net = require('net');
-var mongodb = require('mongodb');
+
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+var db = undefined;
+
+MongoClient.connect('mongodb://localhost:27017/airobot', function(err, _db) {
+    assert.equal(null, err);
+    debug("Connected successfully to server");
+    db = _db;
+});
 
 var isDebug = true;
 
@@ -10,23 +20,63 @@ function debug(msg) {
 
 var userSocket = undefined, operatorSocket = undefined;
 var scriptSocket = new net.Socket();
+var queueToSendToScript = [];
 
 function tryConnect() {
     scriptSocket.connect(12345, '127.0.0.1', function() {
         debug('Connected to Tolya\'s script');
+        sendDataToScript();
     });
+}
+
+function sendDataToScript() {
+    //KOSTIL, because Tolya's script closes connection after each request
+    if (queueToSendToScript.length > 0)
+        scriptSocket.write(queueToSendToScript[0]);
+}
+
+function processAnswerFromScript(json) {
+    debug('Data from script: ' + json);
+    var data = JSON.parse(json);
+
+    var nearestQuestion = data.result['similar_questions'][0].question;
+    var p = data.result['similar_questions'][0].probability;
+    if (p < 0.2) {
+        debug("Cannot find question with ai");
+        userSocket.write(JSON.stringify({"question": data.result.question, "ok": false, "origin": "ai"}));
+    } else {
+        debug("looking for question in db: " + nearestQuestion);
+
+        db.collection('questions')
+            .find({'question': nearestQuestion})
+            .toArray(function (err, docs) {
+                debug(docs)
+                if (docs.length > 0) {
+                    debug("Found answer in db: " + docs[0].answer);
+                    userSocket.write(JSON.stringify(
+                        {
+                            "question": nearestQuestion,
+                            "original_question": data.result.question,
+                            'answer': docs[0].answer,
+                            "ok": data.success,
+                            "origin": "ai"
+                        }
+                    ));
+                } else {
+                    userSocket.write(JSON.stringify({"question": data.result.question, "ok": false, "origin": "ai"}));
+                }
+            });
+    }
 }
 
 scriptSocket.on('error', function(err) {
     debug(err);
-    setTimeout(tryConnect, 3000);
+    setTimeout(tryConnect, 1000);
 });
 
 scriptSocket.on('data', function(json) {
-    debug('Data from script: ' + json);
-    var data = JSON.parse(json);
-
-    //userSocket.write(JSON.stringify({"question": data.question, "ok": false, "origin": "ai"}));
+    queueToSendToScript.shift();
+    processAnswerFromScript(json);
 });
 
 tryConnect();
@@ -38,9 +88,8 @@ function processUserQuery(json) {
         if (operatorSocket != undefined)
             operatorSocket.write(json);
     } else if (data.target == 'ai') {
-        scriptSocket.write(JSON.stringify({'action': 'get', 'input': data.question}));
-        //tmp
-        userSocket.write(JSON.stringify({"question": data.question, "ok": false, "origin": "ai"}));
+        queueToSendToScript.push(JSON.stringify({'action': 'get', 'input': data.question}));
+        sendDataToScript();
     } else {
         debug("Shiiiet");
     }
